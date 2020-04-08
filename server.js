a2d9533e6497f0a5797c9d2279cd82f5c58bdcf9
Blair <abrgroup@gmail.com>
2020-04-08 19:56:07 -0400

    MVP complete - added both PATCH and PUT requests not sure if we need both?

--- server.js
+++ server.js
@@ -60,6 +60,45 @@ server.delete("/api/users/:id", (req, res) => {
   }
 });
 
+server.patch("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+
+  if (user) {
+    if (req.body.name || req.body.bio) {
+      const updatedUser = db.updateUser(user.id, {
+        name: req.body.name,
+        bio: req.body.bio
+      });
+      res.status(200).json(updatedUser);
+    } else {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be modified." });
+    }
+  } else {
+    res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+
+  if (user) {
+    const updatedUser = db.updateUser(user.id, {
+      name: req.body.name || user.name,
+      bio: req.body.bio || user.bio
+    });
+
+    res.json(updatedUser);
+  } else {
+    res.status(404).json({
+      message: "User not found"
+    });
+  }
+});
+
 server.listen(8080, () => {
   console.log("server started at port 8080");
 });
